4294fb4678943622f6d03777107d63fb0c40daba
jdrenteria <jesusrenteria48@gmail.com>
2019-12-17 01:35:01 -0600

    added schema file and connected to database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burgers_name,devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burgers_name,devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burgers_name,devoured) VALUES ('Pizza Burger', FALSE);
+
+
+
+
+
