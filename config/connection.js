ff50c0c3e83834cd58552b5fe33abcdba509b96d
jdrenteria <jesusrenteria48@gmail.com>
2019-12-17 01:38:58 -0600

    Finished connection file to be able to connect to sql

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection){
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3000,
     user: "root",
     password: "password",
     database: "burgers_db"
-}
+});
+
+connection.connect(function(err){
+    if (err){
+        console.error("error connection:" + err.stack);
+        return;
+    }
+    console.log("connnected as id " + connection.threadId);
+});
+
+module.exports=connection;
